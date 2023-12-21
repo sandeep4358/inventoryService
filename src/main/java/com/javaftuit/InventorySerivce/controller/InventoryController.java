594f04bd29e68f1b4d3750e1879e749ef386d046
sandeep4358 <sandeep4358@gmail.com>
2023-12-21 23:24:18 +0530

    add the resiliance fall back mechanishm

--- src/main/java/com/javaftuit/InventorySerivce/controller/InventoryController.java
+++ src/main/java/com/javaftuit/InventorySerivce/controller/InventoryController.java
@@ -2,9 +2,11 @@ package com.javaftuit.InventorySerivce.controller;
 
 import com.javaftuit.InventorySerivce.dto.InventoryResponse;
 import com.javaftuit.InventorySerivce.service.InventoryService;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,10 +25,15 @@ public class InventoryController {
     // http://localhost:8082/api/inventory?skuCode=iphone_13&skuCode=iphone_13_red
     @GetMapping(value = "/isInStock")
     @ResponseStatus(HttpStatus.OK)
+    @CircuitBreaker(name = "inventory", fallbackMethod = "customFallBackMethod")
     public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode) {
         log.info("enter");
         logger.info("Received inventory check request for skuCode: {}", skuCode);
         return inventoryService.isInStock(skuCode);
+    }
+
+    public ResponseEntity customFallBackMethod(List<String> skuCode, RuntimeException exception){
+        return new ResponseEntity("Oops Something went wrong, please check the order with cutomer care.",HttpStatus.CONFLICT);
     }
 
     @GetMapping(value = "/getInventory")
